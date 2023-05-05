826875e58c9d8a8422bb3c5f559fa44668be8da2
damian <damian97tata@gmail.com>
2023-05-05 17:22:37 -0300

    2023-05-05

--- tp1/ejercicio1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir ejercicio_bash
-cd ejercicio_bash
-nano ejercicio.txt
-echo "hola, mundo!" > ejercicio.txt
-mkdir subcarpeta
-cp ejercicio.txt subcarpeta
--- tp1/ejercicio2.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-mkdir ejercicio_bash_avanzado
-cd ejercicio_bash_avanzado
-wget https://github.com/td3-frm/practica/raw/master/01-linea-de-comando/hola.zip
-unzip hola.zip
-mv hola.c hola_mundo.c
-gcc -o hola_mundo hola_mundo.c
-./hola_mundo
